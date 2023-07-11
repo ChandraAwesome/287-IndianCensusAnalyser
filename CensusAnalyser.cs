b6336886e87c82c9535a9d76f6837e8afc6d826b
ChandarAwesome <ajaygames00Gmail.com>
2023-07-11 10:59:58 +0530

    [Chandra]-IndianCensusData

---
+++ CensusAnalyser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indian_State_Census_Analyser
+{
+    public class CensusAnalyser
+    {
+        public static int a = 10;
+        public enum Country
+        {
+            INDIA, US, BRAZIL
+        }
+
+
+        Dictionary<string, DTO.CensusDTO> dataMap;
+        public Dictionary<string, DTO.CensusDTO> LoadCensusData(string csvFilePath, Country country, string dataHeaders)
+        {
+            //CensusAnalyser obj = new CensusAnalyser();
+            //Console.WriteLine(obj.a);
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return dataMap;
+        }
+
+    }
+
+}
---
+++ IndianCensusAdapter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indian_State_Census_Analyser
+{
+    class IndianCensusAdapter : CensusAdapter
+    {
+        string[] censusData;
+        Dictionary<string, DTO.CensusDTO> dataMap;
+        public Dictionary<string, DTO.CensusDTO> LoadCensusData(string csvFilePath, string dataHeaders)
+        {
+            dataMap = new Dictionary<string, DTO.CensusDTO>();
+            censusData = GetCensusData(csvFilePath, dataHeaders);
+            foreach (string data in censusData.Skip(1))
+            {
+                if (!data.Contains(","))
+                {
+                    throw new CensusAnalyserException("File contains wrong delimiter", CensusAnalyserException.ExceptionType.INCORRECT_DELIMITER);
+
+                }
+                string[] column = data.Split(",");
+                if (csvFilePath.Contains("IndiaStateCode.csv"))
+                    dataMap.Add(column[1], new DTO.CensusDTO(new POCO.StateCodeDAO(column[0], column[1], column[2], column[3])));
+                if (csvFilePath.Contains("IndiaStateCensusData.csv"))
+                    dataMap.Add(column[0], new DTO.CensusDTO(new POCO.CensusDataDAO(column[0], column[1], column[2], column[3])));
+
+            }
+
+            return dataMap.ToDictionary(p => p.Key, p => p.Value);
+        }
+    }
+
+}
